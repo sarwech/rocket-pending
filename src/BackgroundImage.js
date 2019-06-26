dfe7fb5e72a811482d521ea4741c9a8fc2a0ed9e
Sarwech Shar <sarwech@Sarwechs-MacBook-Pro-2.local>
2019-06-26 16:13:53 +0100

    set body with image as background instead of a div

--- src/BackgroundImage.js
+++ src/BackgroundImage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import image from './electron.jpg';
+// import image from './electron.jpg';
 import './App.css';
 
 class BackgroundImage extends Component {
     render() {
         return (
             <div className="container">
-                <img className="rocket-image" src={image} alt="Rocket" />
+                {/* <img className="rocket-image" src={image} alt="Rocket" /> */}
             </div>
         )
     }
