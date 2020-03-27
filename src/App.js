f4c7bd9fd7f7197c3627579145fd413729ba70e1
Sarwech Shar <sarwech.shar@gmail.com>
2020-03-27 17:18:27 +0000

    Fix images

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import BackgroundImage from './components/BackgroundImage';
-import LaunchInfo from './components/LaunchInfo';
-import Countdown from './Countdown';
-import './App.css';
+import BackgroundImage from './components/BackgroundImage'
+import LaunchInfo from './components/LaunchInfo'
+import Countdown from './Countdown'
+import electron from './images/electron.png'
+import atlas from './images/atlas_v.png'
+import soyuz from './images/soyuz.png'
+import falcon from './images/falcon.png'
+import proton from './images/proton.png'
+
+import './App.css'
 
 const ROCKETSLIST = [
-  { id: 0, name: 'Electron', src: './images/electron_1.jpg' },
-  { id: 1, name: 'Atlas', src: './images/atlas-v.jpg' },
-  { id: 2, name: 'Soyuz', src: 'soyuz.jpeg' },
-  { id: 3, name: 'Falcon', src: 'falcon.jpeg' },
-  { id: 4, name: 'Proton / UR-500', src: 'Electron.jpeg' }
-];
+	{ id: 0, name: 'Electron', src: electron },
+	{ id: 1, name: 'Atlas', src: './images/atlas_v.png' },
+	{ id: 2, name: 'Soyuz', src: 'soyuz.png' },
+	{ id: 3, name: 'Falcon', src: 'falcon.png' },
+	{ id: 4, name: 'Proton / UR-500', src: 'proton.png' }
+]
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState('');
-  const [rockets, setRockets] = useState(ROCKETSLIST);
+	const [loading, setLoading] = useState(false)
+	const [data, setData] = useState('')
+	const [rockets, setRockets] = useState(ROCKETSLIST)
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://launchlibrary.net/1.3/launch/next/1`)
-      .then(response => response.json())
-      .then(launch => {
-        const nextLaunch = launch.launches[0];
-        setData(nextLaunch);
-        setRockets(
-          rockets.find(rocket => rocket.name === nextLaunch.rocket.familyname)
-        );
-      })
-      .catch(error => {
-        console.log(`There was a problem getting the launch data`, error);
-      });
-    setLoading(false);
-  }, []);
+	useEffect(() => {
+		setLoading(true)
+		fetch(`https://launchlibrary.net/1.3/launch/next/1`)
+			.then(response => response.json())
+			.then(launch => {
+				const nextLaunch = launch.launches[0]
+				setData(nextLaunch)
+				setRockets(
+					rockets.find(rocket => rocket.name === nextLaunch.rocket.familyname)
+				)
+			})
+			.catch(error => {
+				console.log(`There was a problem getting the launch data`, error)
+			})
+		setLoading(false)
+	}, [])
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="App">
-      <BackgroundImage loading={loading} image={rockets.src} />
-      <LaunchInfo data={data} />
-      <Countdown time={data.net} />
-    </div>
-  );
-};
+	if (loading) {
+		return <div>Loading...</div>
+	}
+	return (
+		<div className='App'>
+			<BackgroundImage loading={loading} image={rockets.src} />
+			<LaunchInfo data={data} />
+			<Countdown time={data.net} />
+		</div>
+	)
+}
 
-export default App;
+export default App
--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-import '../App.css';
+import '../App.css'
 
 const BackgroundImage = props => {
-  if (props.loading && props.image !== undefined) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="container">
-        {console.log(props.image)}
-        <img className="rocket-image" src={props.image} alt="Rocket" />
-      </div>
-    );
-  }
-};
+	if (props.loading && props.image !== undefined) {
+		return <div>Loading...</div>
+	} else {
+		return (
+			<div className='container'>
+				{console.log(props.image)}
+				<img className='rocket-image cover' src={props.image} alt='Rocket' />
+			</div>
+		)
+	}
+}
 
-export default BackgroundImage;
+export default BackgroundImage
